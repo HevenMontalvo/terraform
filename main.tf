66f4f60643ca97064f15b4c93bb26ddf45bd1b2e
heven <evn.montalvo@gmail.com>
2026-04-09 13:13:03 -0600

    first ec2 resource

---
+++ main.tf
@@ -0,0 +1,12 @@
+/* Any comment here */
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# This is a single-line comment.
+
+resource "aws_instance" "base" {
+  ami           = "ami-0d729a60"
+  instance_type = "t2.micro"
+}
