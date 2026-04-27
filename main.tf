0cc9c6e18d11829957a16a948f326fee2f82c9b2
Elvin Martinez <evn.montalvo@gmail.com>
2026-04-27 17:09:40 -0600

    Provisioning a simple web server

--- main.tf
+++ main.tf
@@ -7,10 +7,30 @@ provider "aws" {
 # This is a single-line comment.
 
 resource "aws_instance" "base" {
-  ami           = "ami-0d729a60"
-  instance_type = "t2.micro"
+  ami                    = "ami-0d729a60"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.sg_instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello my little world" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  user_data_replace_on_change = true
 
   tags = {
     Name = "example-3"
+  }
+}
+
+resource "aws_security_group" "sg_instance" {
+  name = "terraform-sg-istance-example"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
